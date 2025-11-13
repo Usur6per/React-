19d453cb0ad9458fd7ef7b9d1aec3e89609f4550
Usur6per <intouch.cha@spumail.net>
2025-11-14 01:10:28 +0700

    delete source code

--- src/components/AppFooter.jsx
+++
@@ -1,9 +0,0 @@
-const AppFooter = () => {
-    return ( 
-    <div className="d-flex bg-dark justify-content-center mt-5">
-        <h2 className="text-light">มหาวิทยาลัยศรีปทุม คณะเทคโนโลยีสารสนเทศ สาขาวิทยาการคอมพิวเตอร์ FB:INTOUCH CHAIPETCH IG:usu6rper</h2>
-    </div>
-    );
-}
- 
-export default AppFooter;
